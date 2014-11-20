97aaa3f0c45a69ffd770c97cd6da1ef7aa81f011
LibertysYarn <code@libertysyarn.com>
2014-11-20 13:31:16 -0500

    reviews scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141120020337) do
+ActiveRecord::Schema.define(version: 20141120181711) do
 
   create_table "movies", force: true do |t|
     t.string   "title"
@@ -26,6 +26,14 @@ ActiveRecord::Schema.define(version: 20141120020337) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "tating"
+    t.text     "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
